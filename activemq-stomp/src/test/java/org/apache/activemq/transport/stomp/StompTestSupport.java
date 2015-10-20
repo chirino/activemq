46072dab7cfd92ec35f97893e0f91780c2e2911b
Dejan Bosanac <dejan@nighttale.net>
2015-10-20 12:30:46 +0200

    https://issues.apache.org/jira/browse/AMQ-6013 - init serializable packages statically
    (cherry picked from commit e100638244c4ca5eb2a1f16bcdc671c9859c2694)

--- activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/StompTestSupport.java
+++ activemq-stomp/src/test/java/org/apache/activemq/transport/stomp/StompTestSupport.java
@@ -111,7 +111,6 @@ public class StompTestSupport {
     }
 
     public void startBroker() throws Exception {
-        System.setProperty("org.apache.activemq.SERIALIZABLE_PACKAGES", "*");
         createBroker(true);
 
         XStreamBrokerContext context = new XStreamBrokerContext();
